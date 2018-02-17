d1091a42bdfc811aa223242ca8590edf9d540a1a
artur_fjodorov <artur_fjodorov@mail.ru>
2018-02-17 16:03:42 +0300

    Sequelize test

--- server/sequelize/sequelize.ts
+++ server/sequelize/sequelize.ts
@@ -2,5 +2,5 @@ import {Sequelize} from 'sequelize-typescript';
 const config = require('./../config/config.json');
 
 // dynamic configuration depending on env
-const env = process.env.NODE_ENV; // || 'development';
+const env = process.env.NODE_ENV || 'test';
 export const sequelize = new Sequelize(config[env]).addModels([`${__dirname}/../models`]);
