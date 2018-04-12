42250bbf3364780d6497d3758014642ffbd6d478
artur_fjodorov <artur.fodorov@gmail.com>
2018-04-12 14:59:18 +0300

    Components templates

---
+++ client/src/components/common/Chip/Chip.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="chip">{{ name }}</div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+
+  export default Vue.extend({
+    props: {
+      name: {
+        type: String,
+        required: true,
+        default: ''
+      }
+    }
+  });
+</script>
+
+<style lang="scss" scoped>
+  @import '../../../styles/variables';
+</style>
---
+++ client/src/components/technologies/technology-picker/TechnologyPicker.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script lang="ts">
+  import {Vue} from "vue/types/vue";
+
+  export default Vue.extend({
+
+  })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
