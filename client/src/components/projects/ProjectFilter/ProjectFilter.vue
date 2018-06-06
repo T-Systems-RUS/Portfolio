8bbe809129e8945304f7c0a0a38d19e9bd1540dc
Igogrek <igogrek@gmail.com>
2018-06-06 15:36:50 +0300

    Added radio buttons

--- client/src/components/projects/ProjectFilter/ProjectFilter.vue
+++ client/src/components/projects/ProjectFilter/ProjectFilter.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="filter">
+    <Sorting/>
     <Accordion
       :name="model.name"
       :opened="model.opened"
@@ -30,14 +31,12 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import Accordion from '../../common/Accordion/Accordion.vue';
-  import Checkbox from '../../common/Checkbox/Checkbox.vue';
+  import Sorting from './Sorting.vue';
   import TechnologyPicker from '../../technologies/TechnologyPicker/TechnologyPicker.vue';
   import {IProjectFilter, IProjectFilterCheck} from '../../../shared/interfaces/shared/IProjectFilter';
-  import {IModel} from '../../../shared/interfaces/IModel';
   import {Util} from '../../../shared/classes/Util';
   import {FETCH_ADDONS} from '../../../store/modules/projects/action-types';
-  import {ADDONS, PROJECT_FILTER} from '../../../store/modules/projects/getter-types';
+  import {PROJECT_FILTER} from '../../../store/modules/projects/getter-types';
   import {SET_FILTER, SET_ACCORDION} from '../../../store/modules/projects/mutation-types';
 
   export default Vue.extend({
@@ -56,9 +55,8 @@
       }
     },
     components: {
-      Accordion,
-      Checkbox,
-      TechnologyPicker
+      TechnologyPicker,
+      Sorting
     },
     mounted() {
       this.models.forEach(item => this.$store.commit(SET_ACCORDION, {key: item.name, value: true}));
@@ -69,7 +67,7 @@
       handleFilterAction(item: IProjectFilterCheck, key: string) {
         this.$store.commit(SET_FILTER, {key: Util.mapNameToProperty(key), value: item.id});
       },
-      toggleAccordion(key:string, value: boolean) {
+      toggleAccordion(key: string, value: boolean) {
         this.$store.commit(SET_ACCORDION, {key, value});
       }
     }
---
+++ client/src/components/projects/ProjectFilter/Sorting.vue
@@ -0,0 +1,55 @@
+<template>
+  <Accordion
+    name="Sorting"
+    :opened="sortingOpened"
+    @update:opened="sortingOpened = !sortingOpened">
+    <div class="radio-button-wrapper">
+      <RadioButton
+        :name="'sorting'"
+        :checked="true"
+        :label="'Name'"/>
+    </div>
+    <div class="radio-button-wrapper">
+      <RadioButton
+        :name="'sorting'"
+        :checked="true"
+        :label="'Production line'"/>
+    </div>
+    <div class="radio-button-wrapper">
+      <RadioButton
+        :name="'sorting'"
+        :checked="true"
+        :label="'Team size'"/>
+    </div>
+    <div class="radio-button-wrapper">
+      <RadioButton
+        :name="'sorting'"
+        :checked="true"
+        :label="'Date modified'"/>
+    </div>
+  </Accordion>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+
+  export default Vue.extend({
+    data() {
+      return {
+        sortingOpened: true
+      };
+    },
+    created() {
+    },
+    methods: {}
+  });
+</script>
+
+<style lang="scss" scoped>
+  @import '../../../styles/variables';
+
+  .radio-button-wrapper {
+    margin-bottom: 4px;
+  }
+</style>
