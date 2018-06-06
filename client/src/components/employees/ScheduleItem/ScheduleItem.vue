ce7cf4ab37f5b648405149d76b6cca14a0ee7865
artur_fjodorov <artur.fodorov@gmail.com>
2018-06-06 16:05:05 +0300

    Schedule Item and style fixes

---
+++ client/src/components/employees/ScheduleItem/ScheduleItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="list-item">
+    <img class="list-item-image" src="../assets/person.svg"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {ISchedule} from "../../shared/interfaces/ISchedule";
+
+  export default Vue.extend({
+    props: {
+      employee: {
+        type: {} as () => ISchedule,
+        required: true
+      }
+    }
+  });
+</script>
+
+<style lang="scss" scoped>
+  @import '../../styles/variables';
+
+
+</style>
