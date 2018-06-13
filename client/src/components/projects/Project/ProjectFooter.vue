74b576af54e22a6fa1d415a0bb80bd001d23947c
Igogrek <igogrek@gmail.com>
2018-06-13 17:05:24 +0300

    Refactoring of current footer

--- client/src/components/projects/Project/ProjectFooter.vue
+++ client/src/components/projects/Project/ProjectFooter.vue
@@ -4,25 +4,25 @@
       to="edit"
       append
       class="footer-button">
-      <img src="../assets/edit.svg">
+      <img src="./assets/edit.svg">
       <span class="title is-5 is-size-16 is-uppercase is-block">Edit</span>
     </router-link>
+    <!--<a class="footer-button">-->
+    <!--<img src="../assets/copy.svg">-->
+    <!--<span class="title is-5 is-size-16 is-uppercase is-block">Create copy</span>-->
+    <!--</a>-->
+    <!--<a class="footer-button">-->
+    <!--<img src="../assets/history.svg">-->
+    <!--<span class="title is-5 is-size-16 is-uppercase is-block">Project history</span>-->
+    <!--</a>-->
     <a class="footer-button">
-      <img src="../assets/copy.svg">
-      <span class="title is-5 is-size-16 is-uppercase is-block">Create copy</span>
-    </a>
-    <a class="footer-button">
-      <img src="../assets/history.svg">
-      <span class="title is-5 is-size-16 is-uppercase is-block">Project history</span>
-    </a>
-    <a class="footer-button">
-      <img src="../assets/image.svg">
-      <span class="title is-5 is-size-16 is-uppercase is-block">Upload image</span>
+      <img src="./assets/presentation.svg">
+      <span class="title is-5 is-size-16 is-uppercase is-block">Presentation</span>
     </a>
     <a
-      @click="emitAction('delete')"
+      @click="deleteProject()"
       class="footer-button">
-      <img src="../assets/trash.svg">
+      <img src="./assets/trash.svg">
       <span class="title is-5 is-size-16 is-uppercase is-block">Delete project</span>
     </a>
   </footer>
@@ -32,8 +32,8 @@
 
   export default Vue.extend({
     methods: {
-      emitAction(action:string) {
-        this.$emit(action);
+      deleteProject() {
+        this.$emit('delete');
       }
     }
   });
@@ -51,14 +51,14 @@
     background-color: $white;
 
     &-button {
-      flex:1;
+      flex: 1;
       text-align: center;
 
       img {
+        width: 24px;
         margin-bottom: 5px;
       }
     }
-
 
   }
 </style>
