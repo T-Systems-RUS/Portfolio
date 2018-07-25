e170fc4778dc60665669186a309e3fa86c11ad66
artur_fjodorov <artur.fodorov@gmail.com>
2018-07-25 12:58:35 +0200

    Create button deleted legacy client

--- legacy-client/src/app/shared/trimtext.directive.ts
+++
@@ -1,9 +0,0 @@
-import {Directive, ElementRef} from '@angular/core';
-
-@Directive({selector: '[trimtext]'})
-
-export class TrimTextDirective {
-  constructor(el: ElementRef) {
-    console.log(el.nativeElement.innerText);
-  }
-}
